30d92d8338b1e24f9724a59e8ee3499f8ed9b27c
henriarm <henriquegabrielyan@gmail.com>
2024-10-12 17:01:02 +0300

    Remove unused libs

--- calc_demand.py
+++ calc_demand.py
@@ -1,8 +1,4 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, r2_score
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 
